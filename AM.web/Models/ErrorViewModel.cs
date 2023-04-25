1c74ff06499514896997550666e65f5df626f25a
NaftiSirine <naftisirin30@gmail.com>
2023-04-25 14:20:30 +0100

    add-AM.web

---
+++ AM.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AM.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
